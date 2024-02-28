d1c3d1bf362e40858c68fee4cdd6624a2124b8d5
dimash-git <dimash9i@yandex.com>
2024-02-28 13:52:56 +0600

    fix: tailwindcss config

--- vite-project/src/Components/SideBar.jsx
+++ vite-project/src/Components/SideBar.jsx
@@ -1,43 +1,43 @@
-import {HiOutlineMenuAlt2, HiOutlineHome} from 'react-icons/hi'
-import {CiShoppingCart, CiDeliveryTruck} from 'react-icons/ci'
-import {IoHeartOutline} from 'react-icons/io5'
+import { HiOutlineMenuAlt2, HiOutlineHome } from "react-icons/hi";
+import { CiShoppingCart, CiDeliveryTruck } from "react-icons/ci";
+import { IoHeartOutline } from "react-icons/io5";
 
 const SideBar = () => {
   return (
-    <div className='fixed top-0 let-0 h-screen p-2   bg-gray-100'>
-      <ul className='p-5 space-y-8'>
+    <div className="fixed top-0 let-0 h-screen p-2 bg-gray-100">
+      <ul className="p-5 space-y-8">
         <li>
-        <button>
-          <HiOutlineMenuAlt2 size={'1.5rem'} />
-        </button>
+          <button>
+            <HiOutlineMenuAlt2 size={"1.5rem"} />
+          </button>
         </li>
 
         <li>
-        <button>
-          <HiOutlineHome size={'1.5rem'}/>
-        </button>
+          <button>
+            <HiOutlineHome size={"1.5rem"} />
+          </button>
         </li>
 
         <li>
-        <button>
-          <CiShoppingCart size={'1.5rem'}/>
-        </button>
+          <button>
+            <CiShoppingCart size={"1.5rem"} />
+          </button>
         </li>
 
         <li>
-        <button>
-          <IoHeartOutline size={'1.5rem'}/>
-        </button>
+          <button>
+            <IoHeartOutline size={"1.5rem"} />
+          </button>
         </li>
 
         <li>
-        <button>
-          <CiDeliveryTruck size={'1.5rem'}/>
-        </button>
+          <button>
+            <CiDeliveryTruck size={"1.5rem"} />
+          </button>
         </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
